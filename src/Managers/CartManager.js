26bed995ac005947431651bae4969e439cf2f20c
chivi <paulofr016@gmail.com>
2023-05-03 15:33:57 -0300

    fixes 1, not working

---
+++ src/Managers/CartManager.js
@@ -0,0 +1,99 @@
+import { readFile, writeFile } from 'fs/promises';
+
+let cartIdCounter = 1;
+
+class CartManager {
+  async createCart(cartPostman, response) {
+    try {
+      const carts = JSON.parse(await readFile('carts.json'));
+
+      const newCart = {
+        id: cartIdCounter++,
+        cartPostman,
+        products: []
+      };
+
+      carts.push(newCart);
+
+      await writeFile('carts.json', JSON.stringify(carts));
+
+      response.status(201).json(newCart);
+    } catch (err) {
+      console.error(err);
+      response.sendStatus(500);
+    }
+  }
+
+  async getCartById(id) {
+    try {
+      const carts = await this.readCartsFile();
+  
+      const cart = carts.find((c) => c.id === id);
+      return cart;
+    } catch (error) {
+      console.error(error);
+      throw new Error('Error while getting the cart.');
+    }
+  }
+  
+  async readCartsFile() {
+    try {
+      const cartsBuffer = await readFile(path.join(process.cwd(), 'carts.json'));
+      const carts = JSON.parse(cartsBuffer.toString());
+      return carts;
+    } catch (error) {
+      console.error(error);
+      throw new Error('Error while reading carts file.');
+    }
+  }
+
+  async addToCart(cartId, productId) {
+    try {
+      // Leer el archivo de carts
+      const cartsBuffer = await readFile(path.join(process.cwd(),'carts.json'));
+      const carts = JSON.parse(cartsBuffer.toString());
+  
+      // Buscar el carrito correspondiente al ID proporcionado
+      const cart = carts.find((c) => c.id === cartId);
+  
+      if (!cart) {
+        throw new Error('Cart not found.');
+      }
+  
+      // Leer el archivo de products
+      const productsBuffer = await readFile(path.join(process.cwd(),'products.json'));
+      const products = JSON.parse(productsBuffer.toString());
+  
+      // Buscar el producto correspondiente al ID proporcionado
+      const product = products.find((p) => p.id === productId);
+  
+      if (!product) {
+        throw new Error('Product not found.');
+      }
+  
+      // Buscar si el producto ya está en el carrito y actualizar la cantidad si es el caso
+      const existingProduct = cart.products.find((item) => item.id === product.id);
+  
+      if (existingProduct) {
+        existingProduct.quantity++;
+      } else {
+        // Agregar el producto al carrito con una cantidad de 1
+        cart.products.push({
+          id: product.id,
+          quantity: 1
+        });
+      }
+  
+      // Escribir los carts actualizados en el archivo
+      await writeFile('carts.json', JSON.stringify(carts));
+  
+      return cart;
+    } catch (error) {
+      console.error(error);
+      throw new Error('Error while adding product to cart.');
+    }
+  }
+}
+
+export default CartManager;
+
--- src/Routers/carts.router.js
+++ src/Routers/carts.router.js
@@ -1,103 +1,32 @@
-import express  from 'express';
 import { Router } from "express";
-import { readFile, writeFile } from 'fs/promises';
-import path from 'path';
-import fs from "fs";
+import { createCart, getCartById, addToCart } from '../Managers/CartManager.js';
+
 
 const router = Router();
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-const app = express();
-app.use(express.json());
 
-//crear un nuevo carrito
-  let cartIdCounter = 1;
-  router.post('/', async (req, res) => {
-    try {
-      const carts = JSON.parse(await readFile('carts.json'));
-      const cartPostman = req.body;
-      // Crear un nuevo carrito con un id autoincrementable
-      const newCart = {
-        id: cartIdCounter++,
-        cartPostman,
-        products: []
-      };
-  
-      // Agregar el nuevo carrito al array de carts
-      carts.push(newCart);
-  
-      // Escribir los carts actualizados en el archivo
-      await writeFile('carts.json', JSON.stringify(carts));
-  
-      res.status(201).json(newCart);
-    } catch (err) {
-      console.error(err);
-      res.sendStatus(500);
-    }
-  });
-  
-  // Endpoint para agregar un producto a un carrito
-  router.post('/:cid/product/:pid', async (req, res) => {
-    try {
-      // Leer el archivo de carts
-      const carts = JSON.parse(await readFile('carts.json'));
-
-      // Buscar el carrito correspondiente al cid especificado
-      const cart = carts.find((cart) => cart.id === parseInt(req.params.cid));
-  
-      if (!cart) {
-        return res.sendStatus(404);
-      }
-  
-      // Leer el archivo de products
-      const products = JSON.parse(await readFile('products.json'));
-  
-      // Buscar el producto correspondiente al pid especificado
-      const product = products.find((product) => product.id === parseInt(req.params.pid));
-  
-      if (!product) {
-        return res.sendStatus(404);
-      }
-  
-      // Buscar si el producto ya está en el carrito y actualizar la cantidad si es el caso
-      const existingProduct = cart.products.find((item) => item.id === product.id);
-  
-      if (existingProduct) {
-        existingProduct.quantity++;
-      } else {
-        // Agregar el producto al carrito con una cantidad de 1
-        cart.products.push({
-          id: product.id,
-          quantity: 1
-        });
-      }
-  
-      // Escribir los carts actualizados en el archivo
-      await writeFile('carts.json', JSON.stringify(carts));
-  
-      res.status(200).json(cart);
-    } catch (err) {
-      console.error(err);
-      res.sendStatus(500);
-    }
-  });
-
-/// Función para obtener un carrito por su ID
-async function getCartById(id) {
+router.post('/', async (req, res) => {
   try {
-    // Leer el archivo "carts.json"
-    const cartsBuffer = await readFile(path.join(process.cwd(),'carts.json'));
-    const carts = JSON.parse(cartsBuffer.toString());
+    await createCart(req.body, res);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+});
 
-    // Buscar el carrito correspondiente al ID proporcionado
-    const cart = carts.find((c) => c.id === id);
-    return cart;
+router.post('/:cid/product/:pid', async (req, res) => {
+  try {
+    const cartId = parseInt(req.params.cid);
+    const productId = parseInt(req.params.pid);
+  
+    const cart = await addToCart(cartId, productId);
+  
+    res.status(200).json(cart);
   } catch (error) {
     console.error(error);
-    throw new Error('Error while getting the cart.');
+    res.status(500).json({ error: 'Internal server error' });
   }
-}
+});
 
-// Ruta para obtener un carrito por su ID
 router.get('/:cid', async (req, res) => {
   try {
     const cartId = parseInt(req.params.cid);
@@ -113,25 +42,5 @@ router.get('/:cid', async (req, res) => {
     res.status(500).json({ error: 'Internal server error' });
   }
 });
-
-  
-/* // Ruta para obtener los productos de un carrito por su ID
-router.get('/:cid/products', async (req, res) => {
-  try {
-  // Obtener el ID del carrito de la URL
-  const { cid } = req.params;
-
-  // Buscar el carrito correspondiente en el archivo "carrito.json"
-  const cartFound = await getCartById(cid);
-
-  // Devolver los productos del carrito
-  res.json(cartFound.products);
-} catch (error) {
-  console.error(error);
-  res.status(500).send('Hubo un error al obtener los productos del carrito.');
-}
-}); */
-
-
 
 export default router;
