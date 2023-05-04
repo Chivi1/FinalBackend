9317b099ab986d21fd06d47b3640d7ad75c8356b
chivi <paulofr016@gmail.com>
2023-05-03 21:59:35 -0300

    cartManager Working

--- src/Routers/carts.router.js
+++ src/Routers/carts.router.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { createCart, getCartById, addToCart } from '../Managers/CartManager.js';
+import CartManager from '../Managers/CartManager.js';
 
 
 const router = Router();
 
 router.post('/', async (req, res) => {
   try {
-    await createCart(req.body, res);
+    await CartManager.createCart(req.body, res);
   } catch (err) {
     console.error(err);
     res.sendStatus(500);
@@ -18,7 +18,7 @@ router.post('/:cid/product/:pid', async (req, res) => {
     const cartId = parseInt(req.params.cid);
     const productId = parseInt(req.params.pid);
   
-    const cart = await addToCart(cartId, productId);
+    const cart = await CartManager.addToCart(cartId, productId);
   
     res.status(200).json(cart);
   } catch (error) {
@@ -30,7 +30,7 @@ router.post('/:cid/product/:pid', async (req, res) => {
 router.get('/:cid', async (req, res) => {
   try {
     const cartId = parseInt(req.params.cid);
-    const cart = await getCartById(cartId);
+    const cart = await CartManager.getCartById(cartId);
 
     if (!cart) {
       return res.status(404).json({ error: 'Cart not found' });
